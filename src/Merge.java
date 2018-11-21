a0a4a270a0cbf6a1f1f5fb46329cddf7e0d278ee
BT_1N3_09 <mmoore4262@bths.edu>
2018-11-21 10:15:45 -0500

    ko

--- src/Merge.java
+++ src/Merge.java
@@ -12,28 +12,18 @@ public class Merge {
         int[] temp= new int[n];
         mergeSortHelper(arr,0,n-1,temp);
     }
-    /**public static void mergeSortHelper(int[] arr, int left, int right, int[] temp)
+
+    public static void mergeSortHelper(int[] arr, int left, int right, int[] temp)
     {
         if (left<right)
         {
-            int middleElement= (left+right)/2;
-            for (int i=0; i<temp.length; i++)
-            {
-                System.out.print(arr[i]);
-            }
-            System.out.println("");
-            mergeSortHelper(arr,left,middleElement,temp);
-
-            for (int i=0; i<temp.length; i++)
-            {
-                System.out.print(arr[i]);
-            }
-            System.out.println("");
-            mergeSortHelper(arr,middleElement+1,right,temp);
-
-            //merge(arr,left,middleElement,right,temp);
+            int middleOfArr=(left+right)/2;
+            mergeSortHelper(arr,left,middleOfArr,temp);
+            mergeSortHelper(arr,middleOfArr+1,right,temp);
+            //merge(arr,left,middleOfArr,right,temp);
         }
-    }**/
+    }
+
     public static void merge(int[] arr, int left, int middleOfArr, int right, int[] temp)
     {
         int j=0;
@@ -43,19 +33,10 @@ public class Merge {
             {
                 //Should the index of temp be a variable
                 temp[j]=arr[right+i];
-                if (arr[])
+                //if (arr[])
             }
         }
 
     }
-    public static void mergeSortHelper(int[] arr, int left, int right, int[] temp)
-    {
-        if (left<right)
-        {
-            int middleOfArr= (left+right)/2;
-            mergeSortHelper(arr,left,middleOfArr,temp);
-            mergeSortHelper(arr,middleOfArr+1,right,temp);
-            //merge(arr,left,middleOfArr,right,temp);
-        }
-    }
+
 }
